6b267c2331427e33eedc77bbd336d17383df5229
Nina Desnica <ninche91@gmail.com>
2023-01-17 18:16:38 +0100

    Add files via upload

---
+++ CompliancePolicy/CompliancePolicy_Import_FromJSON.ps1
@@ -0,0 +1,91 @@
+﻿Import-Module Microsoft.Graph.Devices.CorporateManagement
+
+<# region Authentication
+To authenticate, you'll use the Microsoft Graph PowerShell SDK. If you haven't already installed the SDK, see this guide:
+https://learn.microsoft.com/en-us/powershell/microsoftgraph/installation?view=graph-powershell-1.0 
+The PowerShell SDK supports two types of authentication: delegated access, and app-only access. 
+For details on using delegated access, see this guide here:
+https://learn.microsoft.com/powershell/microsoftgraph/get-started?view=graph-powershell-1.0
+For details on using app-only access for unattended scenarios, see Use app-only authentication with the Microsoft Graph PowerShell SDK:
+https://learn.microsoft.com/powershell/microsoftgraph/app-only?view=graph-powershell-1.0&tabs=azure-portal 
+#>
+
+$ImportPath = Read-Host -Prompt "Please specify a path to a JSON file to import data from e.g. C:\IntuneOutput\Policies\policy.json"
+
+# Replacing quotes for Test-Path
+$ImportPath = $ImportPath.replace('"', '')
+
+# Check if the path exists
+if (!(Test-Path "$ImportPath")) {
+    Write-Host "Import Path for JSON file doesn't exist..." -ForegroundColor Red
+    Write-Host "Script can't continue..." -ForegroundColor Red
+    Write-Host
+    break
+}
+
+#function to test if the JSON is valid
+Function Test-JSON() {
+
+    <#
+    .SYNOPSIS
+    This function is used to test if the JSON passed to a REST Post request is valid
+    .DESCRIPTION
+    The function tests if the JSON passed to the REST Post is valid
+    .EXAMPLE
+    Test-JSON -JSON $JSON
+    Test if the JSON is valid before calling the Graph REST interface
+    .NOTES
+    NAME: Test-JSON
+    #>
+    
+    param ( 
+        $JSON  
+    )
+    
+    try {
+        $TestJSON = ConvertFrom-Json $JSON -ErrorAction Stop
+        $validJson = $true  
+    }
+    
+    catch {
+        $validJson = $false
+        $_.Exception
+    }
+    
+    if (!$validJson) {
+        Write-Host "Provided JSON isn't in valid JSON format" -f Red
+        break
+    }
+}
+
+$JSON_Data = Get-Content $ImportPath
+
+# Excluding entries that are not required - id,createdDateTime,lastModifiedDateTime,version
+$JSON_Convert = $JSON_Data | ConvertFrom-Json | Select-Object -Property * -ExcludeProperty id, createdDateTime, lastModifiedDateTime, version, deployedAppCount
+
+$DisplayName = $JSON_Convert.displayName
+
+$JSON_Output = $JSON_Convert | ConvertTo-Json -Depth 5
+
+# Adding Scheduled Actions Rule to JSON
+$scheduledActionsForRule = '"scheduledActionsForRule":[{"ruleName":"PasswordRequired","scheduledActionConfigurations":[{"actionType":"block","gracePeriodHours":0,"notificationTemplateId":"","notificationMessageCCList":[]}]}]'        
+
+$JSON_Output = $JSON_Output.trimend("}")
+
+$JSON_Output = $JSON_Output.TrimEnd() + "," + "`r`n"
+
+# Joining the JSON together
+$JSON_Output = $JSON_Output + $scheduledActionsForRule + "`r`n" + "}"
+            
+write-host
+write-host "Compliance Policy '$DisplayName' Found..." -ForegroundColor Cyan
+write-host
+$JSON_Output
+write-host
+Write-Host "Adding Compliance Policy '$DisplayName'" -ForegroundColor Yellow
+
+$CreateResult = New-MgDeviceManagementDeviceCompliancePolicy -BodyParameter $JSON_Output
+
+Write-Host "Policy created with id" $CreateResult.id
+
+
