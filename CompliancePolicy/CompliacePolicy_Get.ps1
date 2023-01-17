9468a22caac55e2569aea7632aa048e63e85f049
Nina Desnica <ninche91@gmail.com>
2023-01-17 12:36:57 +0100

    Create CompliacePolicy_Get.ps1

---
+++ CompliancePolicy/CompliacePolicy_Get.ps1
@@ -0,0 +1,26 @@
+Import-Module Microsoft.Graph.Devices.CorporateManagement
+
+<# region Authentication
+To authenticate, you'll use the Microsoft Graph PowerShell SDK. If you haven't already installed the SDK, see this guide:
+https://learn.microsoft.com/en-us/powershell/microsoftgraph/installation?view=graph-powershell-1.0 
+The PowerShell SDK supports two types of authentication: delegated access, and app-only access. 
+For details on using delegated access, see this guide here:
+https://learn.microsoft.com/powershell/microsoftgraph/get-started?view=graph-powershell-1.0
+For details on using app-only access for unattended scenarios, see Use app-only authentication with the Microsoft Graph PowerShell SDK:
+https://learn.microsoft.com/powershell/microsoftgraph/app-only?view=graph-powershell-1.0&tabs=azure-portal 
+#>
+
+####################################################
+
+# Using get-mgdeviceappmanagementDeviceCompliancePolicy to get compliance policies
+
+$DCPs = Get-MgDeviceManagementDeviceCompliancePolicy
+
+write-host
+
+foreach($DCP in $DCPs){
+
+write-host "Device Compliance Policy:"$DCP.displayName -f Yellow
+$DCP
+
+}
