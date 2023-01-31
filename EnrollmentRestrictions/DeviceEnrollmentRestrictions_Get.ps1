8ca42eac01b1f77a9ca2db314287ffd5ea06c78c
Nina Desnica <ninche91@gmail.com>
2023-01-31 12:05:46 +0100

    Create DeviceEnrollmentRestrictions_Get.ps1

---
+++ EnrollmentRestrictions/DeviceEnrollmentRestrictions_Get.ps1
@@ -0,0 +1,24 @@
+Import-Module Microsoft.Graph.Devices.CorporateManagement
+
+<# region Authentication
+To authenticate, you'll use the Microsoft Graph PowerShell SDK. If you haven't already installed the SDK, see this guide:
+https://learn.microsoft.com/en-us/powershell/microsoftgraph/installation?view=graph-powershell-1.0 
+The PowerShell SDK supports two types of authentication: delegated access, and app-only access. 
+For details on using delegated access, see this guide here:
+https://learn.microsoft.com/powershell/microsoftgraph/get-started?view=graph-powershell-1.0
+For details on using app-only access for unattended scenarios, see Use app-only authentication with the Microsoft Graph PowerShell SDK:
+https://learn.microsoft.com/powershell/microsoftgraph/app-only?view=graph-powershell-1.0&tabs=azure-portal 
+#>
+
+# Using Get-MgDeviceManagementDeviceEnrollmentConfiguration to get enrollment restrictions
+
+$DeviceEnrollmentConfigurations = Get-MgDeviceManagementDeviceEnrollmentConfiguration
+$DeviceEnrollmentConfigurations | Where-Object ($_.id).contains("DefaultPlatformRestrictions") 
+
+write-host
+
+foreach($DEC in $DeviceEnrollmentConfigurations){
+
+write-host "Enrollment Restriction:"$DEC.displayName -f Yellow
+$DEC
+}
