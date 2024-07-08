252b9e1e9806c5d51c08a65ecc22efc4e5fe3e10
Mark Hopper <mahopper@microsoft.com>
2024-07-08 15:05:06 -0500

    Add files via upload

--- AppProtectionPolicy/ManagedAppPolicy_Export.ps1
+++ AppProtectionPolicy/ManagedAppPolicy_Export.ps1
@@ -19,7 +19,7 @@ $AndroidPolicies = Get-MgDeviceAppManagementAndroidManagedAppProtection -Propert
 $iOSPolicies = Get-MgDeviceAppManagementiOSManagedAppProtection -Property Id, DisplayName, Description
 
 if ($AndroidPolicies.Length -eq 0 -and $iOSPolicies.Length -eq 0) {
-    Write-Host "No policies found" -ForegroundColor Red
+    Write-Host "No policies found" -ForegroundColor Yellow
     break
 }
 
@@ -29,13 +29,10 @@ $AndroidPolicies | Format-Table -Property Id, DisplayName, Description
 Write-Host "iOS Policies:"
 $iOSPolicies | Format-Table -Property Id, DisplayName, Description
 
-Write-Host
 $ExportPath = Read-Host -Prompt "Please specify a path to export each policy's JSON file to e.g. C:\IntuneOutput"
 
 # If the directory path doesn't exist prompt user to create the directory
 if (!(Test-Path "$ExportPath")) {
-    
-    Write-Host
     Write-Host "Path '$ExportPath' doesn't exist, do you want to create this directory? Y or N?" -ForegroundColor Yellow
 
     $Confirm = Read-Host
@@ -55,38 +52,37 @@ if (!(Test-Path "$ExportPath")) {
 }
 
 #Loop through each Android policy and export the JSON file
-Write-Host "Exporting Android policies..." -ForegroundColor Green
-Write-Host
-foreach ($Policy in $AndroidPolicies.Id) {
+Write-Host "Exporting Android policies..."
+foreach ($Policy in $AndroidPolicies) {
+    $Uri = "https://graph.microsoft.com/beta/deviceAppManagement/androidManagedAppProtections('$($Policy.Id)')" + '?$expand=apps'
+    $ExportedFilePath = "$ExportPath\ManagedAppPolicy_$($Policy.DisplayName)_$($Policy.Id).json"
     try {
-        $Policy = Get-MgDeviceAppManagementAndroidManagedAppProtection -AndroidManagedAppProtectionId $Policy
+        $PolicyBody = Invoke-MgGraphRequest -Method GET -Uri $Uri -ContentType JSON -OutputFilePath "$ExportPath\ManagedAppPolicy_$($Policy.DisplayName)_$($Policy.Id).json"
     }
     catch {
-        Write-Host "An error occurred while retrieving the Managed App Policy with the id '$Id', please provide a valid policy id..." -f Red
+        Write-Host "An error occurred while retrieving the Managed App Policy with the id $Policy.Id, please provide a valid policy id..." -f Red
         break
     }
-    $Policy | ConvertTo-Json -Depth 10 | Out-File "$ExportPath\ManagedAppPolicy_$($Policy.DisplayName)_$($Policy.Id).json"
     if ($? -eq $true) {
-        Write-Host "JSON file created and can be found at $ExportPath\ManagedAppPolicy_$($Policy.DisplayName)_$($Policy.Id).json" -ForegroundColor Green
+        Write-Host "JSON file created: $ExportedFilePath" -ForegroundColor Green
     }
 }
 
 Write-Host
-Write-Host "Exporting iOS policies..." -ForegroundColor Green
-Write-Host
+Write-Host "Exporting iOS policies..." 
 
 #Loop through each iOS policy and export the JSON file
-foreach ($Policy in $iOSPolicies.Id) {
+foreach ($Policy in $iOSPolicies) {
+    $ExportedFilePath = "$ExportPath\ManagedAppPolicy_$($Policy.DisplayName)_$($Policy.Id).json"
+    $Uri = "https://graph.microsoft.com/beta/deviceAppManagement/iosManagedAppProtections('$($Policy.Id)')" + '?$expand=apps'
     try {
-        $Policy = Get-MgDeviceAppManagementiOSManagedAppProtection -IosManagedAppProtectionId $Policy
+        $PolicyBody = Invoke-MgGraphRequest -Method GET -Uri $Uri -ContentType JSON -OutputFilePath "$ExportPath\ManagedAppPolicy_$($Policy.DisplayName)_$($Policy.Id).json"
     }
     catch {
         Write-Host "An error occurred while retrieving the Managed App Policy with the id '$Id', please provide a valid policy id..." -f Red
-        Write-Host
         break
     }
-    $Policy | ConvertTo-Json -Depth 10 | Out-File "$ExportPath\ManagedAppPolicy_$($Policy.DisplayName)_$($Policy.Id).json"
     if ($? -eq $true) {
-        Write-Host "JSON file created and can be found at $ExportPath\ManagedAppPolicy_$($Policy.DisplayName)_$($Policy.Id).json" -ForegroundColor Green
+        Write-Host "JSON file created: $ExportedFilePath" -ForegroundColor Green
     }
 }
